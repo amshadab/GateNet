64cce72eb437d0c92f1ab23698a8dd0cc765c60f
Shadab Ansari <shadabansari8319@gmail.com>
2026-09-13 20:56:33 +0530

    Auth Completed

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,18 +1,24 @@
-from fastapi import APIRouter, Depends, HTTPException, status,Response
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
-from schemas.user_schema import UserRegister,UserResponse,UserLogin,UserLoginResponse
+from schemas.user_schema import UserRegister, UserResponse, UserLogin, UserLoginResponse
 from database import get_session
 from sqlalchemy.exc import SQLAlchemyError
-from services.user_service import create_user,login_user
-from exceptions.user_exception import UsernameAlreadyExistsException,InvalidCredentialsException
+from services.user_service import create_user, login_user
+from exceptions.user_exception import (
+    UsernameAlreadyExistsException,
+    InvalidCredentialsException,
+    UserNotApprovedException,
+)
 from models import User
 from dependencies.auth_dependency import get_current_user
 
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
-@user_router.post("/register", response_model=UserResponse,status_code=status.HTTP_201_CREATED)
-def register_user(user: UserRegister, session:Session=Depends(get_session)):
+@user_router.post(
+    "/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
+def register_user(user: UserRegister, session: Session = Depends(get_session)):
     try:
         new_user = create_user(user, session)
 
@@ -28,38 +34,40 @@ def register_user(user: UserRegister, session:Session=Depends(get_session)):
             detail="Database error Occurred",
         )
 
-@user_router.post("/login",response_model=UserLoginResponse,status_code=status.HTTP_202_ACCEPTED)
-def login(user:UserLogin,response:Response,session:Session=Depends(get_session)):
+
+@user_router.post(
+    "/login", response_model=UserLoginResponse, status_code=status.HTTP_202_ACCEPTED
+)
+def login(user: UserLogin, response: Response, session: Session = Depends(get_session)):
     try:
-        logged_in_user,access_token=login_user(user,session)
+        logged_in_user, access_token = login_user(user, session)
         response.set_cookie(
             key="access_token",
             value=access_token,
             httponly=True,
             secure=False,
             samesite="lax",
-            max_age=30*60
+            max_age=30 * 60,
         )
         return {
-            "access_token":access_token,
-            "token_type":"bearer",
-            "user":logged_in_user
+            "access_token": access_token,
+            "token_type": "bearer",
+            "user": logged_in_user,
         }
-    
+
     except InvalidCredentialsException as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=str(e)
-        )
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    except UserNotApprovedException as e:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(e))
     except SQLAlchemyError:
         session.rollback()
-        
+
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Database error Occurred"
+            detail="Database error Occurred",
         )
 
 
-@user_router.get("/profile")
-def get_profile(current_user:User=Depends(get_current_user)):
+@user_router.get("/profile",response_model=UserResponse)
+def get_profile(current_user: User = Depends(get_current_user)):
     return current_user
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,44 +1,52 @@
 from sqlalchemy.orm import Session
-from utils.security import hash_password,verify_password,create_access_token
+from utils.security import hash_password, verify_password, create_access_token
 from models import User
-from exceptions.user_exception import UsernameAlreadyExistsException,InvalidCredentialsException,UserNotApprovedException
+from exceptions.user_exception import (
+    UsernameAlreadyExistsException,
+    InvalidCredentialsException,
+    UserNotApprovedException,
+)
 
-def create_user(user_data,session:Session):
-    
-    existing_user=session.query(User).filter(User.username==user_data.username).first()
-    
+
+def create_user(user_data, session: Session):
+
+    existing_user = (
+        session.query(User).filter(User.username == user_data.username).first()
+    )
+
     if existing_user:
         raise UsernameAlreadyExistsException()
-    
-    hashed_password=hash_password(user_data.password)
-    
-    new_user=User(
+
+    hashed_password = hash_password(user_data.password)
+
+    new_user = User(
         f_name=user_data.f_name,
         l_name=user_data.l_name,
         username=user_data.username,
-        password_hash=hashed_password
+        password_hash=hashed_password,
+        role="USER",
+        status="PENDING",
     )
-    
+
     session.add(new_user)
     session.commit()
     session.refresh(new_user)
-    
+
     return new_user
 
-def login_user(user_data,session:Session):
-    user=session.query(User).filter(User.username==user_data.username).first()
-    
+
+def login_user(user_data, session: Session):
+    user = session.query(User).filter(User.username == user_data.username).first()
+
     if not user:
         raise InvalidCredentialsException()
-    
-    if not verify_password(user_data.password,user.password_hash):
+
+    if not verify_password(user_data.password, user.password_hash):
         raise InvalidCredentialsException()
-    
-    if user.status!="APPROVED":
+
+    if user.status != "APPROVED":
         raise UserNotApprovedException()
-    
-    access_token=create_access_token(user_id=user.id,username=user.username)
-    
-    
-    
-    return user,access_token
+
+    access_token = create_access_token(user_id=user.id, username=user.username)
+
+    return user, access_token
